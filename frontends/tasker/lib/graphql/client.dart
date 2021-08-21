caed230834cfea083e9e049c89b9ecdbe0bfab7b
jjangga0214 <bnbcmindnpass@gmail.com>
2021-08-21 17:38:50 +0900

    feat(tasker): configure graphql_flutter

--- frontends/tasker/lib/graphql/client.dart
+++ frontends/tasker/lib/graphql/client.dart
@@ -1,17 +1,25 @@
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:flutter/material.dart';
+import '../generated/env.dart';
 
-/// "await initHiveForFlutter();" should be called before calling this function"
 ValueNotifier<GraphQLClient> createGraphQLClient() {
   final httpLink = HttpLink(
-    'https://gnyc-tasker-hasura-transformer.herokuapp.com/',
-  );
+      '$API_ENDPOINT_SCHEME_HTTP://$API_ENDPOINT_IP:$API_ENDPOINT_PORT$API_ENDPOINT_PATH_GRAPHQL');
+
+  final authLink = AuthLink(
+      getToken: () => TASKER_API_ACCESS_TOKEN == false
+          ? null
+          : 'Bearer $TASKER_API_ACCESS_TOKEN');
+
+  final Link link = authLink.concat(httpLink);
 
   return ValueNotifier(
     GraphQLClient(
-      link: httpLink,
+      link: link,
       // The default store is the InMemoryStore, which does NOT persist to disk
-      cache: GraphQLCache(store: HiveStore()),
+      cache: GraphQLCache(),
+      // In order to persist, use HiveStore.
+      // cache: GraphQLCache(store: HiveStore()), // "await initHiveForFlutter();" should be called before calling this function"
     ),
   );
 }
--- frontends/tasker/lib/main.dart
+++ frontends/tasker/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/common.dart'; // import utility methods
 import 'package:kakao_flutter_sdk/user.dart'; // must be imported if version is 0.6.4 or higher
 import 'package:kakao_flutter_sdk/auth.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'generated/env.dart';
+import 'graphql/client.dart';
 
 void main() {
   KakaoContext.clientId = TASKER_OAUTH2_KAKAO_CLIENT_ID;
@@ -13,21 +15,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return GraphQLProvider(
+      client: createGraphQLClient(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
